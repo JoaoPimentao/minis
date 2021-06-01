c8b4c319c3c0de8bef5c40a49687cf382e1bd743
Pimenta <jose.p.oliveira@itsector.pt>
2021-06-01 22:12:47 +0100

    first commit

---
+++ minis/minis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  minis
+//
+//  Created by itsector on 01/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
